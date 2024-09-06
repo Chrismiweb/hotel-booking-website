65ee9d4cc37cc67f59abd378bd59b925f2c598bd
Chrismiweb <chrismibiteso@gmail.com>
2024-09-05 23:56:59 -0700

    fetched api

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from './Sidebar'
+import Uploa from './Upload'
 
 function Dashboard() {
   return (
-    <div className='bg-slate-100'>
+    <div className='bg-slate-100 flex'>
         <Sidebar/>
+        <Uploa/>
     </div>
   )
 }
--- src/dashboard/Upload.jsx
+++ src/dashboard/Upload.jsx
@@ -9,27 +9,36 @@ function Uploa() {
     const [price, setPrice] = useState("")
     const [address, setAddres] = useState("")
 
-    async function uploadHotel(){
+    async function uploadHotel(e){
+        e.preventDefault();
         const baseUrl = "https://hotel-booking-backend-g6je.onrender.com"
         const response = await fetch(baseUrl,{
-            
-            
+            method: "POST",
+            body: JSON.stringify({
+                image,
+                hotelName,
+                price,
+                address
+              }),
+              headers: {
+                "Content-type": "application/json"
+              }})
 
-        })
         .then(res =>res.json())
         if(response){
             console.log(response)
+
         }
     }
 
   return (
     <div>
         <form action="" className='flex flex-col'>
-            <input placeholder='image' type="file" />
-            <input  placeholder='hotelName' type="text" />
-            <input  placeholder='price' type="text" />
-            <input  placeholder='address' type="text" />
-            <button>Upload</button>
+            <input onChange={(e)=>setImage(e.target.value)} placeholder='image' type="file" />
+            <input onChange={(e)=>setHotelName(e.target.value)}  placeholder='hotelName' type="text" />
+            <input onChange={(e)=>setPrice(e.target.value)}   placeholder='price' type="text" />
+            <input onChange={(e)=>setAddres(e.target.value)}   placeholder='address' type="text" />
+            <button onClick={uploadHotel}>Upload</button>
         </form>
     </div>
   )
